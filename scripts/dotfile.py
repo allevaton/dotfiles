99601fc303d380756d1d115da44ea2ceb3092d8e
Nick Allevato <nicholas.allevato@gmail.com>
2014-04-24 00:03:51 -0400

    Some good stuff coming

--- scripts/dotfile.py
+++ scripts/dotfile.py
@@ -20,7 +20,7 @@ class Dotfile(object):
     """
 
     def __init__(self, src, dest, confirm=False, ask_location=False,
-                 add_func=None, ignores=[]):
+                 add_func=None, ignore=None):
         """Creates the Dotfile object with the specified parameters.
 
         Keyword arguments:
@@ -30,59 +30,62 @@ class Dotfile(object):
         confirm -- bool: ask the user whether or not they want to copy this
         ask_location -- bool: ask the user whether or not they want to change
                 the location of this dotfile
-        ignores -- array: an array of files or folders to ignore in a
-                directory
+        ignore -- set: a set of files or folders to ignore in a directory
         """
 
         self.src = src
         self.dest = dest
+        self.ignore = ignore
 
         self.confirm = confirm
         self.ask_location = ask_location
 
     def __str__(self):
+        """Some magic for print handling, mainly
+        """
+
         return self.src
 
-    def copy(self, lst):
+    def copy(self, reverse=False):
         """Goes ahead and copies over the files with the specified file list
 
         Keyword arguments:
+
+        ** TODO change this so it's NOT AN ARRAY
+
         lst -- array of Dotfiles: this is the list created from the main
                 setup.py file.
         """
 
-        source = ''
-
+        # let's make sure we should be continuing anyways
         if self.confirm:
             if not input_confirm():
                 return
 
-        source = os.path.join(self.src_dir, self.src)
+        if not reverse:
+            source = os.path.join(self.src_dir, self.src)
+            shutil.copytree(source, self.dest, ignore=self.ignore)
+        else:
+            # we may need to join with pwd
+            pass
 
-        # start copying files
-
-    def reverse_copy(self, lst):
-        # copy from dest to src
-        pass
-
-    def input_path(self, prompt='a new path'):
+    def input_path(self):
         """Takes the user's input for a path and checks if the location
         exists. If it doesn't, it will ask if it can create the location for
         you.
 
-        Keyword arguments:
-        prompt -- str: allows the user to specify an option for the path,
-                    just in case they don't want the default of:
-                    "a new path"
-                    Note that "Enter " is prepended to the prompt and a
-                    colon (:) is appended to the prompt"""
+        Returns:
+        True if the user allowed the program to create directories, or the
+            directory existed already.
+        False if the user denied to create a directory that didn't exist
+        """
 
         while True:
-            s = input('Enter %s: ' % (prompt))
+            s = input('Enter a new path: ')
 
             if not os.path.exists(s):
                 a = input('The path \"%s\" does not exist, want me to create'
-                          'it? [Y/n]' % (s))
+                          ' it? [Y/n]' % (s))
 
                 if a.lower() == 'y' or a == '':
                     os.mkdir(s)
@@ -93,6 +96,19 @@ class Dotfile(object):
                 return True
 
     def input_confirm(self):
+        """Confirms if the user wants to copy this file.
+        Called based on the self.confirm attribute passed through the
+        constructor
+
+        Why doesn't this have a prompt?
+        It serves basically one purpose, hence why it's named after
+        the constructor argument.
+
+        Returns:
+        True if the user confirmed
+        False if the user denied
+        """
+
         while True:
             s = input('Are you sure you want to copy the file \"%s\"? [Y/n] '
                       % (self.src))
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import argparse
 import os
 import sys
 
-from scripts.dotfile import Dotfile
+from scripts.dotfile import Dotfile, DotfileList
 
 
 def get_list():
@@ -18,14 +18,14 @@ def get_list():
         file_list = [
             Dotfile('vimrc', '~/.vimrc'),
             Dotfile('xinitrc', '~/.xinitrc'),
-            Dotfile('vim', '~/.vim'),
+            Dotfile('vim', '~/.vim', ignore={'bundle', 'tags'}),
             Dotfile('i3', '~/.i3', confirm=True),
         ]
 
     # Windows
     elif os.name == 'nt':
         # u = user profile string in Windows
-        u = '%USERPROFILE'
+        u = '%USERPROFILE%'
 
         file_list = [
             Dotfile('vimrc', '%s/_vimrc' % u),
@@ -38,14 +38,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Copies dotfiles',
                                      prog='dotfiles')
 
-    copy = ''
     parser.add_argument('-c', '--copy', help='copy arguments from '
                         'your computer to the repository', action='count')
     arguments = parser.parse_args()
 
     # if copy argument found
     if arguments.copy is not None:
-        pass
+        # reverse copy the list
+        print('reverse copy time, team!\n')
 
-    for df in get_list():
+    # If we're this far, we'll need the list anyways
+    file_list = get_list()
+
+    for df in file_list:
         print(df)
