0390f8a310bde969922eb7abaa4d011b06705d45
Nicholas Allevato <allevaton@users.noreply.github.com>
2014-08-06 18:21:02 -0400

    Update fix.py

--- fix.py
+++ fix.py
@@ -16,8 +16,8 @@ except:
 
 if __name__ == '__main__':
     if os.name == 'nt':
-        with open('~/_vimrc', 'w') as fp:
-            fp.write('source ~/.vimrc', 'w')
+        with open('_vimrc', 'w') as fp:
+            fp.write('source $HOME/.vimrc', 'w')
 
     if os.path.exists('README.rst'):
         os.remove('README.rst')
