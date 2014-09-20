2c0539a022231a7fc5932b436c5f6b133c0d7717
Nick Allevato <nicholas.allevato@gmail.com>
2014-09-20 16:43:43 -0400

    better fix.py to ensure Vundle.vim is installed

--- fix.py
+++ fix.py
@@ -5,6 +5,7 @@
 
 import os
 import re
+from subprocess import call
 
 
 # This is only python for the cross-platformality.
@@ -14,5 +15,10 @@ if __name__ == '__main__':
     if os.name == 'nt':
         with open('_vimrc', 'w') as fp:
             fp.write('source $HOME/.vimrc')
+
+    if not os.path.exists('.vim/bundle'):
+        os.makedirs('.vim/bundle')
+        call(['git', 'clone', 'https://github.com/gmarik/Vundle.vim.git',
+              '.vim/bundle'])
 
     os.remove(__file__)
