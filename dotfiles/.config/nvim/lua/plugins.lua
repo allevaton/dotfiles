71afc0d6e5354a4528406eb48fd5e919a7300904
Nick Allevato <nicholas.allevato@gmail.com>
2024-08-14 21:39:54 -0400

    Update vim plugins for indentation and some other small things
    Also include a reusable "install-package" function for zshrc

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -49,22 +49,6 @@ return {
     end,
   },
 
-  -- Auto tags and pairs
-  {
-    'windwp/nvim-ts-autotag',
-    config = function()
-      require('nvim-ts-autotag').setup{}
-    end,
-  },
-  {
-    'windwp/nvim-autopairs',
-    config = function()
-      require('nvim-autopairs').setup{
-        disable_filetype = { 'TelescopePrompt', 'vim' },
-      }
-    end,
-  },
-
   -- Snippets
   {
     'L3MON4D3/LuaSnip',
@@ -89,10 +73,8 @@ return {
           'html',
           'javascript',
           'typescript',
-          'tsx'
+          'tsx',
         },
-        indent = { enable = true },
-        autotag = { enable = true },
         highlight = {
           enable = true,
           disable = function(lang, buf)
@@ -103,7 +85,123 @@ return {
             end
           end,
         },
-      }
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            [']m'] = '@function.outer',
+            [']]'] = '@class.outer',
+          },
+          goto_next_end = {
+            [']M'] = '@function.outer',
+            [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[m'] = '@function.outer',
+            ['[['] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[M'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+        },
+      },
+
+      -- nvim-treesitter-refactor
+      refactor = {
+        highlight_definitions = { enable = true },
+        highlight_current_scope = { enable = false },
+        smart_rename = {
+          enable = true,
+          keymaps = {
+            smart_rename = 'grr',
+          },
+        },
+        navigation = {
+          enable = true,
+          keymaps = {
+            -- goto_definition = 'gnd',
+            -- list_definitions = 'gnD',
+            -- list_definitions_toc = 'gO',
+            -- goto_next_usage = '<a-*>',
+            -- goto_previous_usage = '<a-#>',
+          },
+        },
+      },
+
+      -- Autotagging (e.g. in HTML, XML)
+      autotag = {
+        enable = true,
+      },
+
+      -- Autoclosing of tags
+      autopairs = {
+        enable = true,
+      },
+
+      indent = {
+        enable = false,
+      },
+
+      yati = {
+        enable = true,
+        default_lazy = true,
+        default_fallback = 'auto'
+      },
+    }
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'nvim-treesitter/nvim-treesitter-refactor',
+      'windwp/nvim-ts-autotag',
+      'windwp/nvim-autopairs',
+      'yioneko/nvim-yati'
+    },
+  },
+
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {
+      indent = {
+        char = '▏',
+        tab_char = '▏',
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          'help',
+          'alpha',
+          'dashboard',
+          'neo-tree',
+          'Trouble',
+          'lazy',
+          'mason',
+          'notify',
+          'toggleterm',
+          'lazyterm',
+        },
+      },
+    },
+    config = function(_, opts)
+      require('ibl').setup(opts)
+
+      local hooks = require 'ibl.hooks'
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, 'IBLWhitespace', { nocombine = true })
+      end)
     end,
   },
 
@@ -153,7 +251,9 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       require('lualine').setup {
-        options = { theme = 'nord' }
+        options = {
+          theme = 'nord'
+        }
       }
     end,
   },
@@ -162,12 +262,12 @@ return {
   {
     'shaunsingh/nord.nvim',
     config = function()
-      vim.g.nord_contrast = false
-      vim.g.nord_borders = false
-      vim.g.nord_disable_background = true
-      vim.g.nord_italic = false
+      vim.g.nord_contrast                = false
+      vim.g.nord_borders                 = false
+      vim.g.nord_disable_background      = true
+      vim.g.nord_italic                  = false
       vim.g.nord_uniform_diff_background = false
-      vim.g.nord_bold = false
+      vim.g.nord_bold                    = false
     end,
   },
 
@@ -176,4 +276,39 @@ return {
     'wfxr/minimap.vim',
     build = ':!cargo install --locked code-minimap',
   },
+
+  {
+    'nvimtools/none-ls.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local null_ls = require('null-ls')
+      local formatting = null_ls.builtins.formatting
+
+      null_ls.setup({
+        sources = {
+          formatting.prettier.with({
+            filetypes = {
+              'javascript',
+              'typescript',
+              'css',
+              'scss',
+              'html',
+              'json',
+              'yaml',
+              'markdown',
+              'graphql',
+              'md',
+              'txt',
+            },
+          }),
+        },
+      })
+
+      -- Set up format on save
+      -- vim.cmd [[augroup FormatAutogroup]]
+      -- vim.cmd [[autocmd!]]
+      -- vim.cmd [[autocmd BufWritePost * lua vim.lsp.buf.format()]]
+      -- vim.cmd [[augroup END]]
+    end,
+  }
 }
--- dotfiles/.zshrc
+++ dotfiles/.zshrc
@@ -21,6 +21,10 @@ fi
 
 source ~/.zplug/init.zsh
 
+if [ -f ~/.zsh_functions ]; then
+  source ~/.zsh_functions
+fi
+
 if ! zplug check; then
   zplug install
 fi
@@ -34,6 +38,11 @@ else
 
   export OLD_PATH=$PATH
   export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Users/nick/.oh-my-posh:/mnt/c/Users/nick/AppData/Local/Programs/Microsoft\ VS\ Code/bin:/mnt/c/Windows/System32:/mnt/c/Windows:/mnt/c/Windows/System32/WindowsPowerShell/v1.0/
+fi
+
+if ! command -v pygmentize &> /dev/null; then
+  echo "pygmentize not found. Installing..."
+  install_package python3-pygments
 fi
 
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
@@ -89,6 +98,7 @@ if command -v nvim &> /dev/null; then
 
   alias vim='nvim'
   alias vimrc='nvim ~/.config/nvim/init.lua'
+  alias vimrcp='nvim ~/.config/nvim/lua/plugins.lua'
 
   if [ ! -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ]; then
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -111,5 +121,3 @@ bindkey "^[[1;5D" backward-word
 . "$HOME/.cargo/env"
 
 zplug load
-
-#export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
