ca8cb18f0cd6b51df93ca232c5b4c4b0df80aa28
Nick Allevato <nick.allevato@sidelineswap.com>
2025-07-03 16:23:46 -0400

    Add undotree

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -375,4 +375,15 @@ return {
     preset = 'classic',
     options = {},
   },
+  {
+    'jiaoshijie/undotree',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { '<leader>u', '<cmd>lua require(\'undotree\').toggle()<cr>' },
+    },
+    opts = {
+      layout = 'left_bottom',
+    },
+  },
 }
